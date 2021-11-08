448cd9bb61a382b105623fcb4036e034ec131d78
Hip <lehaukiem@gmail.com>
2021-11-08 10:52:43 +0700

    Add project files.

---
+++ kShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using qShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace qShopSolution.Data.Entities
+{
+    public class Category
+    {
+        public int Id { set; get; }
+        public int SortOrder { set; get; }
+        public bool IsShowOnHome { set; get; }
+        public int? ParentId { set; get; }
+        public Status Status { set; get; }
+
+        //public List<ProductInCategory> ProductInCategories { set; get; }
+
+
+        public List<CategoryTranslation> CategoryTranslations { get; set; }
+
+    }
+}
---
+++ kShopSolution.Data/Enums/TransactionStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace qShopSolution.Data.Enums
+{
+    public enum TransactionStatus
+    {
+        Success,
+        Failed
+    }
+}
